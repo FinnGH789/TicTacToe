bdc2aa90663a171178d1563e3dac5de08b3ad1b5
Finnb <finnb@freenet.de>
2023-10-04 14:25:54 +0200

    draw eingebaut

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ for (let i = 0; i < cells.length; i++) {
 }
 
 let X = 0;
+
 function playerChange(remove) {
   if (X % 2 === 0) {
     this.value = "X";
@@ -15,48 +16,51 @@ function playerChange(remove) {
     X++;
     remove.target.removeEventListener("click", playerChange);
   }
-
-  function isBoardFull() {
-    const cells = [a1, a2, a3, b1, b2, b3, c1, c2, c3];
-    for (const cell of cells) {
-      if (cell.value !== "X" && cell.value !== "O") {
-        return false; 
-      }
-    }
-    return true; 
-  }
+  let zahl = 0;
 
   function detectWinner() {
 
-    if (a1.value === "X" && a2.value === "X" && a3.value === "X" ||
-        b1.value === "X" && b2.value === "X" && b3.value === "X" ||
-        c1.value === "X" && c2.value === "X" && c3.value === "X" ||
-        a1.value === "X" && b1.value === "X" && c1.value === "X" ||
-        a2.value === "X" && b2.value === "X" && c2.value === "X" ||
-        a3.value === "X" && b3.value === "X" && c3.value === "X" ||
-        a1.value === "X" && b2.value === "X" && c3.value === "X" ||
-        a3.value === "X" && c1.value === "X" && b2.value === "X") {
+    //Das hier nochmal überarbeiten, weil das ist mist!
+    if (
+        cells[0].value === "X" && cells[1].value === "X" && cells[2].value === "X" ||
+        cells[3].value === "X" && cells[4].value === "X" && cells[5].value === "X" ||
+        cells[6].value === "X" && cells[7].value === "X" && cells[8].value === "X" ||
+        cells[0].value === "X" && cells[1].value === "X" && cells[2].value === "X" ||
+        cells[0].value === "X" && cells[4].value === "X" && cells[8].value === "X" ||
+        cells[2].value === "X" && cells[4].value === "X" && cells[6].value === "X" ||
+        cells[0].value === "X" && cells[3].value === "X" && cells[6].value === "X" ||
+        cells[1].value === "X" && cells[4].value === "X" && cells[7].value === "X" ||
+        cells[2].value === "X" && cells[5].value === "X" && cells[8].value === "X"
+        ) {
+        zahl++;
         alert("Player X has won!");
-        location.reload()
+        location.reload();
     }else if(
-        a1.value === "O" && a2.value === "O" && a3.value === "O" ||
-        b1.value === "O" && b2.value === "O" && b3.value === "O" ||
-        c1.value === "O" && c2.value === "O" && c3.value === "O" ||
-        a1.value === "O" && b1.value === "O" && c1.value === "O" ||
-        a2.value === "O" && b2.value === "O" && c2.value === "O" ||
-        a3.value === "O" && b3.value === "O" && c3.value === "O" ||
-        a1.value === "O" && b2.value === "O" && c3.value === "O" ||
-        a3.value === "O" && c1.value === "O" && b2.value === "O") {
+        cells[3].value === "O" && cells[4].value === "O" && cells[5].value === "O" ||
+        cells[6].value === "O" && cells[7].value === "O" && cells[8].value === "O" ||
+        cells[0].value === "O" && cells[1].value === "O" && cells[2].value === "O" ||
+        cells[0].value === "O" && cells[1].value === "O" && cells[2].value === "O" ||
+        cells[0].value === "O" && cells[4].value === "O" && cells[8].value === "O" ||
+        cells[2].value === "O" && cells[4].value === "O" && cells[6].value === "O" ||
+        cells[0].value === "O" && cells[3].value === "O" && cells[6].value === "O" ||
+        cells[1].value === "O" && cells[4].value === "O" && cells[7].value === "O" ||
+        cells[2].value === "O" && cells[5].value === "O" && cells[8].value === "O"
+      ) {
+        zahl++;
         alert("Player O has won!");
-        location.reload()
+        location.reload();
     }
+    
+  }
 
+  function draw(){
+    if(X === 9 && zahl != 1){
+      alert('Unentschieden, versuchen sie es erneut!');
+      location.reload();
+    }
   }
+
   detectWinner();
-  
-  if (isBoardFull() && !detectWinner()) {
-    alert("Das Spiel ist unentschieden!");
-    location.reload();
-  }
+  draw();
 
 }
